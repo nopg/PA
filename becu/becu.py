faab03c19d6f6d4051cf409d933a1783e822a6a6
Unknown <rgillespie@gmail.com>
2020-04-29 13:49:18 -0600

    push=false

--- becu/becu.py
+++ becu/becu.py
@@ -65,7 +65,7 @@ import api_lib_pa as pa_api
 
 # Using 'python becu.py <filename>' from cli will disable PUSH_CONFIG_TO_PA automatically
 # As well as load security rules from <filename> instead of connecting to a PA
-PUSH_CONFIG_TO_PA = True
+PUSH_CONFIG_TO_PA = False
 
 NEW_PRIVATE_INTRAZONE = "NEW-PRIVATE-ZONE-NAME"
 EXISTING_PRIVATE_ZONES = {
