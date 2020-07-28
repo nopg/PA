0d1de43172d78d7b72547bdae3f10848809b62e6
Unknown <rgillespie@gmail.com>
2020-07-27 23:08:14 -0600

    comment

--- eastwest-helper/zone_settings.py
+++ eastwest-helper/zone_settings.py
@@ -14,7 +14,7 @@ NEW_EASTWEST_ZONE = "DataBase-Servers"        # Name of new zone that servers wi
 EXISTING_TRUST_SUBNET = [
     "10.14.96.0/24",        # Inside subnet(s) that will be moved to the new zone. Must be of type list.
    #"192.168.78.0/24",      # Example if using more than one subnet. Uncomment and update.
-   # SECOND SUBNET NOT YET SUPPORTED --- WILL BE SOON.
+   # SECOND SUBNET NOT OFFICIALLY SUPPORTED --- WILL BE SOON. works but adds extra unnecessary tags.
 ]
 # EXISTING_TRUST_SUBNET = ["192.168.77.14/32"]  # If mask is /32, will check address-objects, not 'any' rules.
 
